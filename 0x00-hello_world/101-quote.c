4e5f6078483f1b106c97a2bcac4a7866b239ed8a
husambe <husambe333@gm>
2022-10-13 15:52:07 -0700

    ff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - prints the standrad error sandwiched around the  standard outpu/t
+ * main - Entry point
  *
- * Return: returns 1
+ * Return: Always o (success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
