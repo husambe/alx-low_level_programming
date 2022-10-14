f3a3b1441c1d97c6e8399faa19c290fc8f948863
husambe <113719077+husambe@users.noreply.github.com>
2022-10-14 12:27:03 -0700

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,7 +10,9 @@ int main(void)
 int i;
 for (i = '0'; i <= '9' ; i++)
 {
-putchar(i);									if (i != '9')									{
+putchar(i);	
+if (i != '9')								
+{
 putchar(',');
 putchar(' ')
 }										}
