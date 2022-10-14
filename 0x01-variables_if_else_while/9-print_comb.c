0f681a1a179b91518da881845e21878d7bbfee70
husambe <husambe333@gm>
2022-10-14 12:21:22 -0700

    f

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,10 @@ int main(void)
 int i;
 for (i = '0'; i <= '9' ; i++)
 {
-putchar(i);
-if (i != '9')
-{
-putchar(',');								
-putchar(',');									}
-}
+putchar(i);									if (i != '9')									{
+putchar(',');
+putchar(' ')
+}										}
 putchar('\n');
 return (0);
 }
