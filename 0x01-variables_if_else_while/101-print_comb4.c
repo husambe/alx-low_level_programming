781d3e92cb8e7110e6f14b10b7d7d7ae8c2a4a2c
husambe <113719077+husambe@users.noreply.github.com>
2022-10-14 13:01:28 -0700

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,49 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
  * main - Entry point
- * Return: Always 0 (Success/correct)
- */
+ *
+ * Description: print all possible different
+ *            combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-int left;
-int right;
-int center;
-for (left = 48; left <= 57; left++)
-{
-for (center = left + 1; center <= 57; center++)
-{
-for (right = center + 1 ; right <= 57; right++)
-{
-putchar(left);
-putchar (center);
-putchar (right);
-if  ((left == 55) && (center == left + 1) && (right == center + 1))
-{
-break;
-}
-putchar(',');
-putchar (' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	int digit1 = 0;
+	int digit2, digit3;
+
+	while (digit1 <= 9)
+	{
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			digit3 = 0;
+			while (digit3 <= 9)
+			{
+				if (digit1 != digit2 &&
+				    digit1 < digit2 &&
+				    digit2 != digit3 &&
+				    digit2 < digit3)
+				{
+					putchar(digit1 + 48);
+					putchar(digit2 + 48);
+					putchar(digit3 + 48);
+
+					if (digit1 + digit2 + digit3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				++digit3;
+			}
+			++digit2;
+		}
+		++digit1;
+	}
+	putchar('\n');
+
+	return (0);
 }
