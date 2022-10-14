f4e3d62627d93c38700b35b6640b41a183e643c3
husambe <husambe333@gm>
2022-10-14 14:53:28 -0700

    l

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,8 +4,8 @@
  * three digits
  * Numbers must be separated by ,followed by a space
  * The two digits must be different
- *  012, 120, 102, 021, 201, 210 are considered the same combination
- *  Return: 0
+ * 012, 120, 102, 021, 201, 210 are considered the same combination
+ * Return: 0
  */
 int main(void)
 {
