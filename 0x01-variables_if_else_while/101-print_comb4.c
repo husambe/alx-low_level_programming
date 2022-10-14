786fe4a439ce3cd4d3242c467e2b4b5bea2bcad3
husambe <113719077+husambe@users.noreply.github.com>
2022-10-14 14:39:43 -0700

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (Success/correct)
+ * main - program that prints all possible different combinations of
+ * three digits
+ * Numbers must be separated by ,followed by a space
+ * The two digits must be different
+ *  012, 120, 102, 021, 201, 210 are considered the same combination
+ *  Return: 0
  */
 int main(void)
 {
-int left;
-int right;
-int center;
-for (left = 48; left <= 57; left++)
+int n1 = 48;
+int n2, n3;
+int com = 44;
+while (n1 <= 57)
 {
-for (center = left + 1; center <= 57; center++)
+n2 = n1 + 1;
+while (n2 <= 57)
 {
-for (right = center + 1 ; right <= 57; right++)
+n3 = n2 + 1;
+while (n3 <= 57)
 {
-putchar(left);
-putchar (center);
-putchar (right);
-if  ((left == 55) && (center == left + 1) && (right == center + 1))
+putchar(n1);
+putchar(n2);
+putchar(n3);
+if (n1 != 55 || n2 != 56 || n3 != 57)
 {
-break;
+putchar(com);
+putchar(32);
 }
-putchar(',');
-putchar (' ');
+n3 += 1;
 }
+n2 += 1;
 }
+n1 += 1;
 }
 putchar('\n');
 return (0);
