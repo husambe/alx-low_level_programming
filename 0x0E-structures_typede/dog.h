9bb6ba8ce200c587c55a5231934fd102f703774c
husambe <113719077+husambe@users.noreply.github.com>
2022-11-14 10:44:40 -0800

    Update dog.h

--- 0x0E-structures_typede/dog.h
+++ 0x0E-structures_typede/dog.h
@@ -2,13 +2,13 @@
 #define DOG_H
 
 /**
- * struct dog - Struct dog
- * @name: name
- * @age: age
- * @owner:  owner
- * Description: This struct is for dog
- */
-
+ * struct dog - dog structure
+ *
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
+ *
+*/
 struct dog
 {
 	char *name;
@@ -16,10 +16,14 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t -Typedef for struct dog
+*/
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*DOG_H*/
+#endif /* struct dog */
