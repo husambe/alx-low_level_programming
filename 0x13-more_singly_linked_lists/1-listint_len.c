f34e853eebe2e3824e128bf3e260794ddf3abb78
husambe <113719077+husambe@users.noreply.github.com>
2022-11-30 01:23:30 +0200

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
- * Return: number of nodes
+ * listint_len -counts number of elements in a linked listint_t
+ * @h: List to check
+ * Return: number of elements in list
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t num = 0;
+	size_t number_of_elements = 0;
+	const listint_t *p = h;
 
-    while (h)
-    {
-        num++;
-        h = h->next;
-    }
-
-    return (num);
+	while (p != NULL)
+	{
+		number_of_elements++;
+		p = p->next;
+	}
+	return (number_of_elements);
 }
