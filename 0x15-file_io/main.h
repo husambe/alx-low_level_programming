c9ab4845fba056d9d5ecdaa33969afbce5ef4d9a
husambe <113719077+husambe@users.noreply.github.com>
2022-12-05 20:19:54 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-file_io directory.
+ */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
